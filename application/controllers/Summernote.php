b6fdfaf3a006480e2e86894e30aae9770f674c3e
Juan Victor <juanvictoroliveira2009@gmail.com>
2020-03-11 15:42:25 -0300

    Incrementando mensagens de erro

--- application/controllers/Summernote.php
+++ application/controllers/Summernote.php
@@ -3,35 +3,76 @@ defined('BASEPATH') or exit('No direct script access allowed');
 
 class Summernote extends CI_Controller
 {
+    /**
+     * Chama a página add.php
+     *
+     * @return void
+     */
     public function index()
     {
         $this->load->view('add');
     }
+    /**
+     * Adiciona uma imagem a pasta uploads
+     *
+     * @return string
+     */
     public function uploadFile()
     {
         if (!$this->input->is_ajax_request()) {
             return false;
         } else {
-            if ($_FILES) {
+            if ($_FILES['file']['name']) {
                 // Set config for the library upload
                 $config = array(
                     'encrypt_name' => TRUE,
                     'upload_path' => './assets/uploads',
                     'allowed_types' => 'png|jpg',
-                    'max_size' => '3000'
+                    'max_size' => '1500'
                 );
 
                 // Load upload library
                 $this->load->library('upload', $config);
 
                 // Verify configs
-                if ($this->upload->do_upload('img')) {
+                if ($this->upload->do_upload('file')) {
                     $img_info = $this->upload->data();
-                    $url = base_url('assets/uploads/').$img_info['file_name'];
-                    return $url;
+                    $url = base_url('assets/uploads/') . $img_info['file_name'];
+
+                    $json =  array(
+                        'error' => 0,
+                        'message' => $url
+                    );
+
+                    echo json_encode($json);
                 } else {
-                    return false;
+                    $json = array(
+                        'error' => 1,
+                        'message' => $this->upload->display_errors()
+                    );
+
+                    echo json_encode($json);
                 }
+            }
+        }
+    }
+    /**
+     * Deleta uma imagem da pasta uploads
+     *
+     * @return string
+     */
+    public function deleteFile()
+    {
+        if (!$this->input->is_ajax_request()) {
+            return false;
+        } else {
+            if ($this->input->post('src') != '') {
+                $url = explode("uploads/", $this->input->post('src'));
+                unlink("./assets/uploads/" . $url[1]);
+                echo 'A imagem foi excluída com sucesso!';
+            } else {
+                echo 'A imagem não foi passada';
+            }
         }
     }
 }
