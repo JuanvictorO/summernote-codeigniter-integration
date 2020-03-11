a82addbeab4c73c6712db13b3fcf240a6d7926b7
Juan Victor <juanvictoroliveira2009@gmail.com>
2020-03-11 10:05:50 -0300

    Upload file function

--- application/controllers/Summernote.php
+++ application/controllers/Summernote.php
@@ -7,4 +7,31 @@ class Summernote extends CI_Controller
     {
         $this->load->view('add');
     }
+    public function uploadFile()
+    {
+        if (!$this->input->is_ajax_request()) {
+            return false;
+        } else {
+            if ($_FILES) {
+                // Set config for the library upload
+                $config = array(
+                    'encrypt_name' => TRUE,
+                    'upload_path' => './assets/uploads',
+                    'allowed_types' => 'png|jpg',
+                    'max_size' => '3000'
+                );
+
+                // Load upload library
+                $this->load->library('upload', $config);
+
+                // Verify configs
+                if ($this->upload->do_upload('img')) {
+                    $img_info = $this->upload->data();
+                    $url = base_url('assets/uploads/').$img_info['file_name'];
+                    return $url;
+                } else {
+                    return false;
+                }
+        }
+    }
 }
