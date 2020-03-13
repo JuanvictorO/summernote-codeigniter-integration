4197e180027f7379ec3e2fb1764caf08c1c854a5
Juan Victor <juanvictoroliveira2009@gmail.com>
2020-03-13 14:36:07 -0300

    Função insert - model

--- application/models/Summernote_model.php
+++ application/models/Summernote_model.php
@@ -3,4 +3,22 @@ defined('BASEPATH') or exit('No direct script access allowed');
 
 class Summernote_model extends CI_Model
 {
+    /**
+     * Undocumented function
+     *
+     * @param [type] $data
+     * @return bool
+     */
+    public function insert($data)
+    {
+        $this->load->database();
+
+        $row = array(
+            'title' => $data['title'],
+            'text' => $data['text'],
+            'update_date' => date('Y-m-d H:i:s')
+        );
+
+        return $this->db->insert('table', $row) ?: false;
+    }
 }
