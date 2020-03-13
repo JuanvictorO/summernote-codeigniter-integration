e1e60f11b42480ab4a9b470b03e4ee40998b70c9
Juan Victor <juanvictoroliveira2009@gmail.com>
2020-03-13 14:35:40 -0300

    Atualizando sql

--- sql/sql.sql
+++ sql/sql.sql
@@ -2,7 +2,7 @@ create database `summernote` default charset utf8;
 use `summernote`;
 create table `table`(
 	`id` int not null primary key auto_increment,
-    `title`varchar(200) not null,
+    `title`varchar(100) not null,
     `text` text not null,
     `update_date` datetime not null
 )engine = InnoDB;
